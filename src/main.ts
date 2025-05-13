4a4466ddddaa3f0687511f3fd4934167a9c9a5bc
DenisSTK <denisdrawler@gmail.com>
2025-05-13 17:12:10 +0300

    cors system

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
     app.use(cookieParser());
 
     app.enableCors({
-        origin: 'http://localhost:5173',
+        origin: 'https://image-generator-woad-five.vercel.app/log-in',
         credentials: true,
     });
 

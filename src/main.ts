14db42021ea3c99d3fdbdd0964cc4b5675559f4b
DenisSTK <denisdrawler@gmail.com>
2025-05-13 17:07:59 +0300

    cors system

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
     app.use(cookieParser());
 
     app.enableCors({
-        origin: '*',
+        origin: 'http://localhost:5173',
         credentials: true,
     });
 

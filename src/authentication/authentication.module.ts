3af02764835e7a671ee3028c85f2c3c028bc0769
DenisSTK <denisdrawler@gmail.com>
2025-05-16 19:09:16 +0300

    removed jwt system

--- src/authentication/authentication.module.ts
+++ src/authentication/authentication.module.ts
@@ -2,13 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
 import { AuthenticationController } from './authentication.controller';
 import { FirebaseModule } from '../firebase/firebase.module';
-import { JwtStrategy } from './jwt.strategy';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [FirebaseModule, ConfigModule],
-  providers: [AuthenticationService, JwtStrategy],
+  providers: [AuthenticationService],
   controllers: [AuthenticationController],
-  exports: [JwtStrategy],
 })
 export class AuthenticationModule {}
